20a0b59f8ac193846c14486b77e31afd0ddf8c90
Gaz Robinson <gaz.j.robinson@gmail.com>
2017-01-22 04:21:11 +0000

    ＬＯＡＤＳ　ＯＦ　ＳＴＵＦＦ Ｂｕｇ　ｆｉｘｅｓ　ｔｏｏ

--- GGJ2017/Assets/RoadGen.cs
+++ GGJ2017/Assets/RoadGen.cs
@@ -7,7 +7,9 @@ public class RoadGen : MonoBehaviour {
 public Transform Car;
 public List<Transform> roadPieces = new List<Transform>();
 public List<Transform> trees = new List<Transform>();
-public float RoadPos = 0.0f;
+public List<Transform> pillars = new List<Transform>();
+public List<Transform> uniques = new List<Transform>();
+    public float RoadPos = 0.0f;
 float roadLength = 100.0f;
     // Use this for initialization
     void Start () {
@@ -20,13 +22,52 @@ float roadLength = 100.0f;
 			RoadPos += roadLength;
         }
     }
-	// Update is called once per frame
-	void Update () {
+    public float treeDrop = 0.25f;
+    // Update is called once per frame
+    void Update () {
         Transform road = GetValidPiece(roadPieces);
 		if(road!=null){
 			road.localPosition = new Vector3(0.0f, 0.0f, RoadPos);
 			RoadPos += roadLength;
 		}
+        Vector3 pos;
+
+        for (int i = 0; i < trees.Count; i++){
+            pos = trees[i].localPosition;
+			
+            float dist = pos.z - Car.position.z;
+			if(dist < -75f){
+                trees[i].localPosition += Vector3.forward * 550f;
+            pos = trees[i].localPosition;
+			dist = pos.z - Car.position.z;
+            }
+			pos.y = 0 - treeDrop * (dist* dist);
+            trees[i].localPosition = pos;
+        }
+        for (int i = 0; i < pillars.Count; i++){
+            pos = pillars[i].localPosition;
+			
+            float dist = pos.z - Car.position.z;
+			if(dist < -75f){
+                pillars[i].localPosition += Vector3.forward * 550f;
+            pos = pillars[i].localPosition;
+			dist = pos.z - Car.position.z;
+            }
+			pos.y = 0 - treeDrop * (dist* dist);
+            pillars[i].localPosition = pos;
+        }
+		for (int i = 0; i < uniques.Count; i++){
+            pos = uniques[i].localPosition;
+			
+            float dist = pos.z - Car.position.z;
+			if(dist < -75f){
+                uniques[i].localPosition += Vector3.forward * 600f;
+            pos = uniques[i].localPosition;
+			dist = pos.z - Car.position.z;
+            }
+			pos.y = 0 - treeDrop * (dist* dist);
+            uniques[i].localPosition = pos;
+        }
     }
 	public Transform GetValidPiece(List<Transform> pieces){
         for (int i = 0; i < pieces.Count; i++)
