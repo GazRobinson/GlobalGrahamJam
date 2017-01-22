ecc05d6d44b38c827047eb6797103e00daaf199d
Gaz Robinson <gaz.j.robinson@gmail.com>
2017-01-22 15:21:13 +0000

    ＦＩＮＡＬＥ

--- GGJ2017/Assets/CamSetup.cs
+++ GGJ2017/Assets/CamSetup.cs
@@ -9,18 +9,20 @@ Camera cam2;
     // Use this for initialization
     void Start () {
         cam = GetComponent<Camera>();
-		cam2 = transform.GetChild(0).GetComponent<Camera>();
+        if (transform.childCount > 0)
+        {
+            cam2 = transform.GetChild(0).GetComponent<Camera>();
+        }
         float current = Screen.width / Screen.height;
 Rect rect = cam.rect;
 rect.width = current / res;
 
         rect.x = (1f - rect.width) * 0.5f;
         cam.rect = rect;
-        cam2.rect = rect;
+        if (cam2 != null)
+        {
+            cam2.rect = rect;
+        }
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
