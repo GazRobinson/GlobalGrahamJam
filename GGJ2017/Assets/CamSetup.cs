21d300aafe8f776ad169f78cd22545b3aebeae7d
Gaz Robinson <gaz.j.robinson@gmail.com>
2017-01-22 15:13:04 +0000

    💦ＳＰＬＡＳＨ　ＳＣＲＥＥＮＳ💦

---
+++ GGJ2017/Assets/CamSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSetup : MonoBehaviour {
+float res = 4f / 3f;
+Camera cam;
+Camera cam2;
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+		cam2 = transform.GetChild(0).GetComponent<Camera>();
+        float current = Screen.width / Screen.height;
+Rect rect = cam.rect;
+rect.width = current / res;
+
+        rect.x = (1f - rect.width) * 0.5f;
+        cam.rect = rect;
+        cam2.rect = rect;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
